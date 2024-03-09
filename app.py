2e8431351ad4a7f70f40348eecc5e5ab3b48ce6d
phudinh153 <95414402+phudinh153@users.noreply.github.com>
2024-03-09 19:33:13 +0700

    cant emit ready

--- app.py
+++ app.py
@@ -13,7 +13,12 @@ def join(message):
     join_room(room)
     print('RoomEvent: {} has joined the room {}\n'.format(username, room))
     emit('ready', {username: username}, to=room, skip_sid=request.sid)
+    print('Emitted ready event to room {} with data: {}'.format(room, {username: username}))
 
+@socketio.on('ack')
+def handle_ack(data):
+    print('Event acknowledged:', data)
+    emit('ack', data, to=request.sid)
 
 @socketio.on('data')
 def transfer_data(message):
